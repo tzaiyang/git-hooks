5e747f59fd0827db4a649cb51b5951770d242fae
tzaiyang <tzaiyang@126.com>
2021-12-04 00:26:32 +0800

    feat: change git hooks directory

--- build.sh
+++ build.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
+set -e
 curl -fsSL https://raw.githubusercontent.com/tzaiyang/git-hooks/master/package.json >package.json
+curl -fsSL https://raw.githubusercontent.com/tzaiyang/git-hooks/master/.gitignore >>.gitignore
 echo "module.exports = {extends: ['@commitlint/config-conventional']}" >commitlint.config.js
 
 # Node env
 npm install
 # Activate hooks
 git init
-npx husky install
-npx husky add .husky/commit-msg 'npx --no -- commitlint --edit "$1"'
+npx husky install .git/hooks
+npx husky add .git/hooks/commit-msg 'npx --no -- commitlint --edit "$1"'
 
