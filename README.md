fe66e3d5cd04e455eb5a43656e6f640d94f2987a
tzaiyang <tzaiyang@126.com>
2021-12-06 14:42:12 +0800

    feat: add talisman to pre-commit

--- README.md
+++ README.md
@@ -9,7 +9,7 @@ Git hooks and tools to validate development specifications such as:
     git config --global init.templateDir $HOME/.git-template
     git clone https://github.com/tzaiyang/git-hooks $HOME/.git-template
     # Check enviorment and dependencies
-    bash $HOME/.git-template/check_env.sh
+    bash $HOME/.git-template/check-dep.sh
     ```
 
    Activate git hooks in your repo:
--- check-dep.sh
+++ check-dep.sh
@@ -22,6 +22,11 @@ if ! command -v standard-version > /dev/null 2>&1 ; then
   echo 'Error: `standard-version` not found. please run `npm install -g standard-version` to install standard-version. Get help: https://github.com/conventional-changelog/standard-version' 1>&2
 fi
 
+# Check talisman
+if ! command -v talisman > /dev/null 2>&1 ; then
+  echo 'Error: `talisman` not found. you can run `brew install talisman` in MacOS X to install talisman. Get help: https://github.com/thoughtworks/talisman#installation-to-a-single-project' 1>&2
+fi
+
 # Check git-flow
 if ! command -v git-flow > /dev/null 2>&1 ; then
   echo 'Warning: `git-flow` not found. Get help: https://github.com/petervanderdoes/gitflow-avh/wiki/Installation' 1>&2
