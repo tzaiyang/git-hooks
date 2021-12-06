e08653a6eee4cb8431815f8d041a6738ec153504
tzaiyang <tzaiyang@126.com>
2021-12-06 10:41:09 +0800

    fix: fix check env bug

--- check_env.sh
+++ check_env.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-set -e
 
 for file in $(ls $HOME/.git-template/hooks|grep -v "configs"); do
   if ! [ -x $HOME/.git-template/hooks/$file ]; then
@@ -10,21 +9,20 @@ done
 
 # Check pre-commit
 if ! command -v pre-commit > /dev/null; then
-  echo 'Error: `pre-commit` not found.  please run `pip install pre-commit` to install pre-commit. \nGet help: https://github.com/pre-commit/pre-commit' 1>&2
-  exit 1
+  echo 'Error: `pre-commit` not found.  please run `pip install pre-commit` to install pre-commit. Get help: https://github.com/pre-commit/pre-commit' 1>&2
 fi
 
 # Check commitlint
-if ! command -v pre-commit > /dev/null; then
-  echo 'Error: `commitlint` not found. please run `npm install -g @commitlint/{config-conventional,cli}` to install commitlint. \nGet help: https://github.com/conventional-changelog/commitlint'
+if ! command -v commitlint > /dev/null; then
+  echo 'Error: `commitlint` not found. please run `npm install -g @commitlint/{config-conventional,cli}` to install commitlint. Get help: https://github.com/conventional-changelog/commitlint' 1>&2
 fi
 
 # Check stand-version
 if ! command -v standard-version > /dev/null 2>&1 ; then
-  echo 'Error: `standard-version` not found. please run `npm install -g standard-version` to install standard-version. \nGet help: https://github.com/conventional-changelog/standard-version' 1>&2
+  echo 'Error: `standard-version` not found. please run `npm install -g standard-version` to install standard-version. Get help: https://github.com/conventional-changelog/standard-version' 1>&2
 fi
 
 # Check git-flow
 if ! command -v git-flow > /dev/null 2>&1 ; then
-  echo 'Warning: `git-flow` not found. \nGet help: https://github.com/petervanderdoes/gitflow-avh/wiki/Installation' 1>&2
+  echo 'Warning: `git-flow` not found. Get help: https://github.com/petervanderdoes/gitflow-avh/wiki/Installation' 1>&2
 fi
